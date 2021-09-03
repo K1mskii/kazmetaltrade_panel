d90db72d7c8d366e4f7969800ccdac1f10f3ff10
K1mskii <movies2702@gmail.com>
2021-09-03 13:33:32 +0600

    finish

---
+++ js/inputMask.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // inputMask
+    let inputs = document.querySelectorAll('input[type="tel"]');
+    let im = new Inputmask("+7 (999) 999-99-99");
+    im.mask(inputs);
+});
---
+++ js/select.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let demo1 = new BVSelect({
+        selector: "#selectbox",
+        width: "490px",
+        searchbox: false,
+        offset: true,
+        placeholder: "Выбрать ",
+        breakpoint: 450
+    });
+    let demo2 = new BVSelect({
+        selector: "#selectbox2",
+        width: "490px",
+        searchbox: false,
+        offset: true,
+        placeholder: "Выбрать роль",
+        breakpoint: 450
+    });
+    let demo3 = new BVSelect({
+        selector: "#selectbox3",
+        width: "490px",
+        searchbox: false,
+        offset: true,
+        placeholder: "Выбрать филиал",
+        breakpoint: 450
+    });
+    let demo4 = new BVSelect({
+        selector: "#selectbox4",
+        width: "490px",
+        searchbox: false,
+        offset: true,
+        placeholder: "Выбрать роль",
+        breakpoint: 450
+    });
+});
